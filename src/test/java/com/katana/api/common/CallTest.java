1f04268b738e7124f6d1eeaf391ceefee0bae95c
Juan Ernesto Garcia Alvarez <jegasmlm@gmail.com>
2016-11-12 14:19:50 +0100

    Call tests

--- src/test/java/com/katana/api/common/CallTest.java
+++ src/test/java/com/katana/api/common/CallTest.java
@@ -1,5 +1,7 @@
 package com.katana.api.common;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,34 +10,87 @@ import static org.junit.Assert.*;
  * Created by juan on 12/11/16.
  */
 public class CallTest {
+    
+    private Call call;
+    
+    @Before
+    public void setup(){
+        this.call = new Call();
+    }
+    
     @Test
-    public void getService() throws Exception {
+    public void getService() {
+        // SETUP
+        String service = "Service";
+        this.call.setService(service);
 
+        // ACTION
+        String serviceObtained = this.call.getService();
+        
+        // EXPECTED
+        Assert.assertEquals(service, serviceObtained);
     }
 
     @Test
-    public void setService() throws Exception {
+    public void setService() {
+        // SETUP
+        String service = "Service";
 
+        // ACTION
+        this.call.setService(service);
+
+        // EXPECTED
+        Assert.assertEquals(service, this.call.getService());
     }
 
     @Test
-    public void getVersion() throws Exception {
+    public void getVersion() {
+        // SETUP
+        String version = "Version";
+        this.call.setVersion(version);
 
+        // ACTION
+        String versionObtained = this.call.getVersion();
+
+        // EXPECTED
+        Assert.assertEquals(version, versionObtained);
     }
 
     @Test
-    public void setVersion() throws Exception {
+    public void setVersion() {
+        // SETUP
+        String version = "Version";
 
+        // ACTION
+        this.call.setVersion(version);
+
+        // EXPECTED
+        Assert.assertEquals(version, this.call.getVersion());
     }
 
     @Test
-    public void getAction() throws Exception {
+    public void getAction() {
+        // SETUP
+        String action = "Action";
+        this.call.setAction(action);
 
+        // ACTION
+        String actionObtained = this.call.getAction();
+
+        // EXPECTED
+        Assert.assertEquals(action, actionObtained);
     }
 
     @Test
-    public void setAction() throws Exception {
+    public void setAction() {
+        // SETUP
+        String action = "Action";
 
+        // ACTION
+        this.call.setAction(action);
+
+        // EXPECTED
+        Assert.assertEquals(action, this.call.getAction());
     }
 
 }
---
+++ src/test/java/com/katana/api/common/HttpRequestTest.java
@@ -0,0 +1,182 @@
+package com.katana.api.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by juan on 12/11/16.
+ */
+public class HttpRequestTest {
+    @Test
+    public void getProtocolVersion() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setProtocolVersion() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getMethod() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setMethod() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getUrl() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setUrl() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getQuery() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setQuery() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getPostData() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setPostData() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getHeaders() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setHeaders() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getBody() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setBody() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getFiles() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setFiles() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void isMethod() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getUrlScheme() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getUrlHost() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getUrlPath() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void hasQueryParam() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getQueryParam() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getQueryParamArray() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getQueryParamsArray() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void hasPostParam() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getPostParam() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getPostParamArray() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getPostParamsArray() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void isProtocolVersion() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void hasHeader() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getHeader() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void hasBody() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void hasFile() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getFile() {
+        Assert.assertTrue(true);
+    }
+
+}
