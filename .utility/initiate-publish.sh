65c1ccc4aaa1bb43b353ab1d6517a8f7f6fdf32a
Juan Ernesto Garcia <jegasmlm@gmail.com>
2017-05-27 00:23:37 +0200

    Fix initiate-publish.sh

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -2,10 +2,10 @@
 # This script initiates the Gradle publishing task when pushes to master occur.
 # NOTE: Travis-CI can only publish SNAPSHOT versions.
 
-if [ "$TRAVIS_REPO_SLUG" == "kusanagi/katana-sdk-java7" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "kusanagi/katana-sdk-java7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Starting publish to Sonatype...\n"
 
-  ./gradlew -Psigning.keyId='$SIGN_KEY' -Psigning.password='$SIGN_PASS' -Psigning.secretKeyRingFile=maven.gpg -PossrhUsername='$SONATYPE_USERNAME' -PossrhPassword='$SONATYPE_PASSWORD' uploadArchives
+  ./gradlew -Psigning.keyId="${SIGN_KEY}" -Psigning.password="${SIGN_PASS}" -Psigning.secretKeyRingFile=maven.gpg -PossrhUsername="${SONATYPE_USERNAME}" -PossrhPassword="${SONATYPE_PASSWORD}" uploadArchives
   RETVAL=$?
 
   if [ $RETVAL -eq 0 ]; then
