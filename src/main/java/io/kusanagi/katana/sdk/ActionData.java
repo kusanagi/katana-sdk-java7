563785cfb4c823281494119f36d58aa3bc67e432
Juan Ernesto Garcia <jegasmlm@gmail.com>
2018-03-02 12:27:10 +0100

    Add copyright to new files

--- src/main/java/io/kusanagi/katana/sdk/ActionData.java
+++ src/main/java/io/kusanagi/katana/sdk/ActionData.java
@@ -1,3 +1,18 @@
+/*
+ * Java 7 SDK for the KATANA(tm) Platform (http://katana.kusanagi.io)
+ * Copyright (c) 2016-2017 KUSANAGI S.L. All rights reserved.
+ *
+ * Distributed under the MIT license
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code
+ *
+ * @link      https://github.com/kusanagi/katana-sdk-java7
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright Copyright (c) 2016-2017 KUSANAGI S.L. (http://kusanagi.io)
+ *
+ */
+
 package io.kusanagi.katana.sdk;
 
 /**
